e1cc035800424d4326aaeb1892806d0049913dac
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:39:58 -0700

    Step 10.9: Only publish tasks the current user can see

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -16,8 +16,14 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  // Only publish tasks that are public or belong to the current user
   Meteor.publish("tasks", function () {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
 }
 
