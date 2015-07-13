907b665f5635995245f7e2188da9a2dd22ab42e0
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:32:59 -0700

    Step 9.3: Update App component to use tasks.insert method

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -24,12 +24,7 @@ class App extends Component {
     // Find the text field via the React ref
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),           // _id of logged in user
-      username: Meteor.user().username,  // username of logged in user
-    });
+    Meteor.call('tasks.insert', text);
 
     // Clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
