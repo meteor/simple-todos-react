3babdf6e68e7324735500a9203460b15915608ce
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:40:55 -0700

    Step 10.10: Add extra security to methods

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -43,10 +43,22 @@ Meteor.methods({
   },
 
   removeTask(taskId) {
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+
     Tasks.remove(taskId);
   },
 
   setChecked(taskId, setChecked) {
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+
     Tasks.update(taskId, { $set: { checked: setChecked} });
   },
 
