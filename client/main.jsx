7c8ec26337c4fff0ba049c8230b81835371c6af7
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:05:08 -0700

    Step 2.3: Replace starter JS

---
+++ client/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+import App from '../imports/ui/App.jsx';
+
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
+});
