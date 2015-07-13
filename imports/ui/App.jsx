0f68a3dea14816f10c88b4d6491612cedd093aaa
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:14:40 -0700

    Step 4.3: Update data container to sort tasks by time

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -58,6 +58,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
