89fe9e41c9f0e73c96fcca5511af582de1cc0428
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:26:59 -0700

    Step 8.2: Create Accounts UI wrapper component

---
+++ imports/ui/AccountsUIWrapper.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+
+export default class AccountsUIWrapper extends Component {
+  componentDidMount() {
+    // Use Meteor Blaze to render login buttons
+    this.view = Blaze.render(Template.loginButtons,
+      ReactDOM.findDOMNode(this.refs.container));
+  }
+  componentWillUnmount() {
+    // Clean up Blaze view
+    Blaze.remove(this.view);
+  }
+  render() {
+    // Just render a placeholder container that will be filled in
+    return <span ref="container" />;
+  }
+}
