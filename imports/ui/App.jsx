8cb48d9c1280f5feda7aff06c385a52dedad832d
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:21:30 -0700

    Step 7.1: Add hide completed checkbox to App component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -35,6 +35,16 @@ class App extends Component {
         <header>
           <h1>Todo List</h1>
 
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
               type="text"
