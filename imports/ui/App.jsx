b595a90b125e15632f84bbe16f7d46e1682418a0
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:13:37 -0700

    Step 4.1: Add form for new tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -18,6 +18,14 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
         <ul>
