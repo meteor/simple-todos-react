e711321a7f138e24b849c17b16e1d229768d0c11
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:32:21 -0700

    Step 10.2: Add methods for add, remove, update task

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -12,3 +12,27 @@ if (Meteor.isClient) {
     React.render(<App />, document.getElementById("render-target"));
   });
 }
+
+Meteor.methods({
+  addTask(text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+
+  removeTask(taskId) {
+    Tasks.remove(taskId);
+  },
+
+  setChecked(taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
