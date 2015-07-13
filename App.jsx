dc0883c23649fb89b4489c97e2f1127c4cfeef2c
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:32:59 -0700

    Step 10.3: Update App component to use addTask method

--- App.jsx
+++ App.jsx
@@ -39,12 +39,7 @@ App = React.createClass({
     // Find the text field via the React ref
     var text = React.findDOMNode(this.refs.textInput).value.trim();
 
-    Tasks.insert({
-      text: text,
-      createdAt: new Date(),            // current time
-      owner: Meteor.userId(),           // _id of logged in user
-      username: Meteor.user().username  // username of logged in user
-    });
+    Meteor.call("addTask", text);
 
     // Clear form
     React.findDOMNode(this.refs.textInput).value = "";
