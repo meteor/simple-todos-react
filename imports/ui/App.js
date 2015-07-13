4afe0bc91fbd9b1b72da00439533b4978d8f7ba9
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:36:59 -0700

    Step 10.5: Update renderTasks to pass in showPrivateButton

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -41,9 +41,18 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
 
   render() {
