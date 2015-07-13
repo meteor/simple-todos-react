ccfc9d51bcdc179ca1040228d73c26aa4ce70e90
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:24:09 -0700

    Step 7.6: Display incompleteCount in the header

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -51,7 +51,7 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
 
           <label className="hide-completed">
             <input
@@ -82,6 +82,7 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
