1b8a7577860c7d9f0f0afa6996cf8fa6fe92d3ec
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:05:58 -0700

    Step 2.4: Create App component

---
+++ App.jsx
@@ -0,0 +1,30 @@
+// App component - represents the whole app
+App = React.createClass({
+  getTasks() {
+    return [
+      { _id: 1, text: "This is task 1" },
+      { _id: 2, text: "This is task 2" },
+      { _id: 3, text: "This is task 3" }
+    ];
+  },
+
+  renderTasks() {
+    return this.getTasks().map((task) => {
+      return <Task key={task._id} task={task} />;
+    });
+  },
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+});
