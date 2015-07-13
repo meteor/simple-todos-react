2fc5338aece0c67370206bd0a53b77e4c94efad7
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:27:36 -0700

    Step 9.3: Include sign in form

--- App.jsx
+++ App.jsx
@@ -68,6 +68,8 @@ App = React.createClass({
             Hide Completed Tasks
           </label>
 
+          <AccountsUIWrapper />
+
           <form className="new-task" onSubmit={this.handleSubmit} >
             <input
               type="text"
