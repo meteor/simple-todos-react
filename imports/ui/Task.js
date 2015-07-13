7b6e746dbc5624bc967c3a0887d8eaacd6eaedc2
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:33:38 -0700

    Step 9.4: Replace update and remove with methods

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
 
 import { Tasks } from '../api/tasks.js';
 
@@ -6,13 +7,11 @@ import { Tasks } from '../api/tasks.js';
 export default class Task extends Component {
   toggleChecked() {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this.props.task._id, {
-      $set: { checked: !this.props.task.checked },
-    });
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
   }
 
   deleteThisTask() {
-    Tasks.remove(this.props.task._id);
+    Meteor.call('tasks.remove', this.props.task._id);
   }
 
   render() {
