0e519b181c0888a67f1424ac5d354c3f78a13c16
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:33:38 -0700

    Step 10.4: Update Task to use setChecked and removeTask methods

--- Task.jsx
+++ Task.jsx
@@ -6,13 +6,11 @@ Task = React.createClass({
 
   toggleChecked() {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this.props.task._id, {
-      $set: {checked: ! this.props.task.checked}
-    });
+    Meteor.call("setChecked", this.props.task._id, ! this.props.task.checked);
   },
 
   deleteThisTask() {
-    Tasks.remove(this.props.task._id);
+    Meteor.call("removeTask", this.props.task._id);
   },
 
   render() {
