54e487630478cd1389a58c08816bd578af1fceaf
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:35:03 -0700

    Step 11.2: Add publish to server and subscribe to client

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -7,9 +7,17 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 
+  Meteor.subscribe("tasks");
+
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.publish("tasks", function () {
+    return Tasks.find();
   });
 }
 
