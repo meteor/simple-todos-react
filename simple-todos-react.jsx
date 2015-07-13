36c59c642fc6a7b48ebe0378c5f75488854479c7
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:05:08 -0700

    Step 2.3: Replace starter JS

---
+++ simple-todos-react.jsx
@@ -0,0 +1,8 @@
+if (Meteor.isClient) {
+  // This code is executed on the client only
+
+  Meteor.startup(function () {
+    // Use Meteor.startup to render the component after the page is ready
+    React.render(<App />, document.getElementById("render-target"));
+  });
+}
