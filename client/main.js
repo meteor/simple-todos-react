4bccdc1930b292f650218daa37b2463c6d19cf3a
Sashko Stubailo <sashko@stubailo.com>
2015-07-13 12:05:08 -0700

    Step 2.3: Replace starter JS

--- client/main.js
+++ client/main.js
@@ -1,22 +1,9 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
 
-import './main.html';
+import App from '../imports/ui/App.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
 });
