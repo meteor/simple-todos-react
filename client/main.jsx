eef345667c247c8adca26d365a9d57fac1ca5c65
Tom Coleman <tom@thesnail.org>
2016-03-18 16:14:08 +1100

    Step 8.5: Import accounts configuration

--- client/main.jsx
+++ client/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
+import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.jsx';
 
 Meteor.startup(() => {
