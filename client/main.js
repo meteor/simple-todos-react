e92d48fc93f2161953b3247a2a4fbc7b99b88333
EdgarAllenPoe <EdgarAllenPoe@users.noreply.github.com>
2018-10-28 07:24:19 -0700

    Update main.js
    correct path to 'imports' directory

--- client/main.js
+++ client/main.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import '../imports/startup/accounts-config.js';
-import App from '../imports/ui/App.js';
+import './imports/startup/accounts-config.js';
+import App from './imports/ui/App.js';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
