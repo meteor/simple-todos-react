cb29eed75a3e752a661219415fb440ec3340ab74
Mariem KHLIFI <mariem.khlifi@telecom-bretagne.eu>
2018-11-03 19:43:25 +0100

    Update main.js
    Adding the missing line.

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import './main.html'; 
 
 import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.js';
