c86362e5c20a9b48bf59d2b52cf320a8b3219e2c
Leonardo Venturini <leovenbag@gmail.com>
2019-12-16 19:17:28 -0400

    Step 1.4: Render Sample Tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Task } from './Task';
+
 
 const tasks = [
   {_id: 1, text: 'First Task'},
@@ -9,5 +11,9 @@ const tasks = [
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+
+    <ul>
+      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+    </ul>
   </div>
 );
