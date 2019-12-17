008dd01084636b00cc395baba6d01c2022030194
Leonardo Venturini <leovenbag@gmail.com>
2019-12-17 15:49:01 -0400

    Step 2.2: Initialize Tasks Collection

--- server/main.js
+++ server/main.js
@@ -1,5 +1,16 @@
 import { Meteor } from 'meteor/meteor';
+import Tasks from '/imports/api/tasks';
 
 Meteor.startup(() => {
-  
+  if (Tasks.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task'
+    ].forEach(insertTask)
+  }
 });
