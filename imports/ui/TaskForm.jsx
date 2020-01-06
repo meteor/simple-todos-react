baebb5b3c6757fd68b2cd71df3df1e1a6105db3c
Leonardo Venturini <leovenbag@gmail.com>
2020-01-06 17:51:19 -0400

    Step 3.2: Add Submit Handler

--- imports/ui/TaskForm.jsx
+++ imports/ui/TaskForm.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
+import Tasks from '/imports/api/tasks';
 
 export const TaskForm = () => {
   const [text, setText] = useState("");
 
+  const handleSubmit = () => {
+    if (!text) return;
+
+    Tasks.insert({
+      text: text.trim(),
+      createdAt: new Date()
+    });
+
+    setText("");
+  };
+
   return (
-    <form className="task-form">
+    <form className="task-form" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Type to add new tasks"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
 
       <button type="submit">Add Task</button>
