5718da4ae074f5383fded2a71f8cf25f9d230d34
Leonardo Venturini <leovenbag@gmail.com>
2020-02-07 19:33:36 -0400

    Step 6.3: Create User Account

--- server/main.js
+++ server/main.js
@@ -2,6 +2,13 @@ import { Meteor } from 'meteor/meteor';
 import Tasks from '/imports/api/tasks';
 
 Meteor.startup(() => {
+  if (!Accounts.findUserByUsername('meteorite')) {
+    Accounts.createUser({
+      username: 'meteorite',
+      password: 'password'
+    });
+  }
+
   if (Tasks.find().count() === 0) {
     [
       'First Task',
