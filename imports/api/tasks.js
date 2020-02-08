e361f4896e83b6ab3b985c5a2d83d8e3be9592f0
Leonardo Venturini <leovenbag@gmail.com>
2020-02-08 08:15:25 -0400

    Step 7.2: Add Task Methods

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,46 @@
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
-export default new Mongo.Collection('tasks');
+export const Tasks = new Mongo.Collection('tasks');
+
+Meteor.methods({
+  'tasks.insert'(text) {
+    check(text, String);
+
+    if (!this.userId) {
+      throw new Meteor.Error('Not authorized.');
+    }
+
+    Tasks.insert({
+      text,
+      createdAt: new Date,
+      owner: this.userId,
+      username: Meteor.users.findOne(this.userId).username
+    })
+  },
+
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+
+    if (!this.userId) {
+      throw new Meteor.Error('Not authorized.');
+    }
+
+    Tasks.remove(taskId);
+  },
+
+  'tasks.setChecked'(taskId, isChecked) {
+    check(taskId, String);
+    check(isChecked, Boolean);
+
+    if (!this.userId) {
+      throw new Meteor.Error('Not authorized.');
+    }
+
+    Tasks.update(taskId, {
+      $set: {
+        isChecked
+      }
+    });
+  }
+});
