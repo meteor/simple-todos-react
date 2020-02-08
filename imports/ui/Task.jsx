cdd75c714ad21768a3b9817fefd13faa4a649617
Leonardo Venturini <leovenbag@gmail.com>
2020-02-07 20:04:17 -0400

    Step 6.9: Task Owner Username

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import classnames from 'classnames';
+import _ from 'lodash';
 
 export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
   const classes = classnames('task', {
     'checked': Boolean(task.isChecked)
   });
 
+  const owner = _.first(Accounts.users.find(task.owner).fetch());
+
   return (
     <li className={classes}>
       <button onClick={ () => onDeleteClick(task) }>&times;</button>
-      <span>{ task.text }</span>
+      <span>{ task.text } { owner && <i>({ owner.username })</i> }</span>
       <input
         type="checkbox"
         checked={ Boolean(task.isChecked) }
