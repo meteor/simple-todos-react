22b738c1ef5c698b6ec70dadd90d2aafb39ec656
Leonardo Venturini <leovenbag@gmail.com>
2020-02-15 12:34:40 -0400

    Step 10.3: Prepare Database

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,8 +1,25 @@
 import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+
+import { Tasks } from './tasks.js';
 
 if (Meteor.isServer) {
   describe('Tasks', () => {
     describe('methods', () => {
+      const userId = Random.id();
+      let taskId;
+
+      beforeEach(() => {
+        Tasks.remove({});
+
+        taskId = Tasks.insert({
+          text: 'Test Task',
+          createdAt: new Date(),
+          owner: userId,
+          username: 'meteorite',
+        });
+      });
+
       it('can delete owned task', () => {
       });
     });
