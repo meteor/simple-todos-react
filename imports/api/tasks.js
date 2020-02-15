3a9ac53752406df5d447dec10dce7388981a202d
Leonardo Venturini <leovenbag@gmail.com>
2020-02-15 07:11:45 -0400

    Step 8.8: Check User Permission

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -22,7 +22,9 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String);
 
-    if (!this.userId) {
+    const task = Tasks.findOne(taskId);
+
+    if (!this.userId || task.owner !== this.userId) {
       throw new Meteor.Error('Not authorized.');
     }
 
@@ -33,7 +35,9 @@ Meteor.methods({
     check(taskId, String);
     check(isChecked, Boolean);
 
-    if (!this.userId) {
+    const task = Tasks.findOne(taskId);
+
+    if (task.isPrivate && task.owner !== this.userId) {
       throw new Meteor.Error('Not authorized.');
     }
 
@@ -48,7 +52,9 @@ Meteor.methods({
     check(taskId, String);
     check(isPrivate, Boolean);
 
-    if (!this.userId) {
+    const task = Tasks.findOne(taskId);
+
+    if (!this.userId || task.owner !== this.userId) {
       throw new Meteor.Error('Not authorized.');
     }
 
