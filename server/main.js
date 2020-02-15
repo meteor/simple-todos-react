a67770d209f6e8220cfe7383e715f16c123b0588
Leonardo Venturini <leovenbag@gmail.com>
2020-02-15 07:15:36 -0400

    Step 8.9: Remove Unneeded Code

--- server/main.js
+++ server/main.js
@@ -1,14 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import Links from '/imports/api/links';
-import { Tasks } from '/imports/api/tasks';
-
-function insertLink(title, url) {
-  Links.insert({ title, url, createdAt: new Date() });
-}
-
-function insertTask(text) {
-  Tasks.insert({ text, createdAt: new Date() });
-}
+import '/imports/api/links';
+import '/imports/api/tasks';
 
 Meteor.startup(() => {
   if (!Accounts.findUserByUsername('meteorite')) {
@@ -16,40 +8,5 @@ Meteor.startup(() => {
       username: 'meteorite',
       password: 'password'
     });
-  }
-
-  if (Tasks.find().count() === 0) {
-    [
-      'First Task',
-      'Second Task',
-      'Third Task',
-      'Fourth Task',
-      'Fifth Task',
-      'Sixth Task',
-      'Seventh Task'
-    ].forEach(insertTask)
-  }
-
-  // If the Links collection is empty, add some data.
-  if (Links.find().count() === 0) {
-    insertLink(
-      'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
-    );
-
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
-
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
-
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
   }
 });
