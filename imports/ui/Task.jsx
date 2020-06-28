fdd697b242df0e7f57ae7265f2b6c9412165324c
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-06-28 16:26:07 -0500

    Step 4.6: Update the Styling in the Task Component

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import classnames from 'classnames';
 
 export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
+  const classes = classnames('task', {
+    'checked': Boolean(task.isChecked)
+  });
+
   return (
-    <li>
+    <li className={classes}>
       <button onClick={ () => onDeleteClick(task) }>&times;</button>
+      <span>{ task.text }</span>
       <input
         type="checkbox"
         checked={ Boolean(task.isChecked) }
         onClick={ () => onCheckboxClick(task) }
         readOnly
       />
-      <span>{ task.text }</span>
     </li>
   );
 };
