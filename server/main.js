572f26e445c0b59c3af9e6d19dbdbcb051434003
theprogboy <mdrezwanferdous@gmail.com>
2020-08-04 11:24:43 +0200

    Added missing insertTask function in step 1.2

--- server/main.js
+++ server/main.js
@@ -2,11 +2,17 @@ import { Meteor } from 'meteor/meteor';
 import Tasks from '/imports/api/tasks';
 
 Meteor.startup(() => {
+  // Function to insert task
+  function insertTask(text) {
+    Tasks.insert({ text, createdAt: new Date() });
+  }
+
+  //Populate DB with some task
   if (Tasks.find().count() === 0) {
     [
       'First Task',
       'Second Task',
-      'Third Task',
+      'Third Task', 
       'Fourth Task',
       'Fifth Task',
       'Sixth Task',
